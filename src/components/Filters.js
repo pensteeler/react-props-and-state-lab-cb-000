9d0569791da62a747aaa1170fe1f496460d2204c
John Vascsinec <pensteeler@gmail.com>
2017-10-21 17:31:47 +0000

    automatically backed up by learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,6 +3,8 @@ import React from 'react';
 class Filters extends React.Component {
   constructor() {
     super();
+
+    this.handleFilterTypeChange = this.handleFilterTypeChange.bind(this);
   }
 
   render() {
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,9 +4,16 @@ import Pet from './Pet';
 
 class PetBrowser extends React.Component {
   render() {
+    // const pets = this.props.pets.map(pet => <Pet onAdoptPet={this.props.onAdoptPet} isAdopted={this.adopted(pet)}/>)
+    //
+    const pets = this.props.pets.map((pet, index) => (
+     <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(pet.id)} />
+    ));
+
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        //<code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {pets}
       </div>
     );
   }
